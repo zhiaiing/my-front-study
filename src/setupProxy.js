d224b82193adc042a9cf0a5809ebb693dfa63ead
kang <kang.ji@ihr360.com>
2021-08-19 16:47:16 +0800

    first commit

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,40 +2,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(
-        '/gateway/component/api',
+        '/gateway/',
         createProxyMiddleware({
-            target: "https://qa2.ihr360.com",
-            changeOrigin: true,
-        })
-    )
-    app.use(
-        '/gateway',
-        createProxyMiddleware({
-            target: 'https://qa2.ihr360.com',
-            changeOrigin: true,
-        })
-    )
-    app.use(
-        '/web/gateway',
-        createProxyMiddleware({
-            target: 'https://qa2.ihr360.com',
-            changeOrigin: true,
-        })
-    )
-    app.use(
-        '/web',
-        createProxyMiddleware({
-            target: 'https://qa2.ihr360.com',
-            changeOrigin: true,
-            pathRewrite: {
-                "^/web": "/gateway/web"
-            }
-        })
-    )
-    app.use(
-        '/api/gateway',
-        createProxyMiddleware({
-            target: 'https://qa2.ihr360.com',
+            target: "https://xxx.xxx.com",
             changeOrigin: true,
         })
     )
